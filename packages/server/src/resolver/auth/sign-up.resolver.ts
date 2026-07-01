543ad088cafb51ddc8f8b1b8f07cf3f516226a49
Chinmay kumar Panda <chinmaykumarpanda004@gmail.com>
2026-07-01 10:55:15 +0530

    fix: auto-verify user emails when VERIFY_USER_EMAIL is false

--- packages/server/src/resolver/auth/sign-up.resolver.ts
+++ packages/server/src/resolver/auth/sign-up.resolver.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, UseGuards } from '@nestjs/common'
 
 import { GraphqlResponse } from '@decorator'
-import { APP_DISABLE_REGISTRATION, BCRYPT_SALT } from '@environments'
+import { APP_DISABLE_REGISTRATION, BCRYPT_SALT, VERIFY_USER_EMAIL } from '@environments'
 import { SignUpInput } from '@graphql'
 import { DeviceIdGuard } from '@guard'
 import { helper } from '@heyform-inc/utils'
@@ -47,7 +47,8 @@ export class SignUpResolver {
       email: input.email,
       password: await passwordHash(input.password, BCRYPT_SALT),
       avatar: gravatar(input.email),
-      lang: client.lang
+      lang: client.lang,
+      isEmailVerified: !VERIFY_USER_EMAIL
     })
 
     this.authService.createUserActivity({
@@ -62,8 +63,10 @@ export class SignUpResolver {
       deviceId: client.deviceId
     })
 
-    const code = await this.authService.getVerificationCodeWithRateLimit(`verify_email:${userId}`)
-    this.mailService.emailVerificationRequest(input.email, code, client.lang)
+    if (VERIFY_USER_EMAIL) {
+      const code = await this.authService.getVerificationCodeWithRateLimit(`verify_email:${userId}`)
+      this.mailService.emailVerificationRequest(input.email, code, client.lang)
+    }
 
     return true
   }
