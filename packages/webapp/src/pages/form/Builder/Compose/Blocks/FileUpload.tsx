b08895770cfc40dd01648f388dfc6affa54bbd63
Chinmay kumar Panda <chinmaykumarpanda004@gmail.com>
2026-07-02 10:51:53 +0530

    fix: update builder compose FileUpload block to dynamically render custom size limits

--- packages/webapp/src/pages/form/Builder/Compose/Blocks/FileUpload.tsx
+++ packages/webapp/src/pages/form/Builder/Compose/Blocks/FileUpload.tsx
@@ -1,6 +1,7 @@
 import { IconChevronRight, IconUpload } from '@tabler/icons-react'
 import type { FC } from 'react'
 import { useTranslation } from 'react-i18next'
+import { useFormStore } from '@/store'
 
 import { FakeSubmit } from '../FakeSubmit'
 import type { BlockProps } from './Block'
@@ -8,6 +9,10 @@ import { Block } from './Block'
 
 export const FileUpload: FC<BlockProps> = ({ field, locale, ...restProps }) => {
   const { t } = useTranslation()
+  const { form } = useFormStore()
+
+  const maxUploadSizeMb = field.properties?.maxUploadSizeMb || form?.maxUploadSizeMb || 10
+  const maxFileSizeStr = `${maxUploadSizeMb}MB`
 
   return (
     <Block className="heyform-file-upload" field={field} locale={locale} {...restProps}>
@@ -15,7 +20,7 @@ export const FileUpload: FC<BlockProps> = ({ field, locale, ...restProps }) => {
         <div className="heyform-upload-wrapper">
           <IconUpload className="heyform-upload-icon non-scaling-stroke" />
           <div className="mt-8">{t('Upload a file or drag and drop', { lng: locale })}</div>
-          <div className="heyform-upload-size-limit">{t('Size limit', { lng: locale })}: 10MB</div>
+          <div className="heyform-upload-size-limit">{t('Size limit', { lng: locale })}: {maxFileSizeStr}</div>
         </div>
       </div>
       <FakeSubmit text={t('Next', { lng: locale })} icon={<IconChevronRight />} />
